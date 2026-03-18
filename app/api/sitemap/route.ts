9ad26c954a048551e73b000a40f4b792aa1dce4e
Pantho <pantho.haque@pathao.com>
2026-03-18 17:27:35 +0600

    fix: enabled api/sitemap

--- app/api/sitemap/route.ts
+++ app/api/sitemap/route.ts
@@ -1,4 +1,5 @@
-<?xml version="1.0" encoding="UTF-8"?>
+export async function GET() {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://postbox-pantho.vercel.app</loc>
@@ -12,4 +13,12 @@
     <changefreq>daily</changefreq>
     <priority>0.8</priority>
   </url>
-</urlset>
+</urlset>`
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, max-age=3600',
+    },
+  })
+}
